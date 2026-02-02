c4a5ad240efaa32b6f6874bf5eb6e203074221b5
13.rvh <chafiikarima003@gmail.com>
2026-02-02 02:48:19 +0100

    h

--- AbsencesReview(For Students).py
+++ AbsencesReview(For Students).py
@@ -1,1 +1,18 @@
+from  customtkinter import * 
 
+
+
+
+app=CTk()
+app.geometry("600x500+400+95")
+app.title("Absences Review (For Students)")
+app._set_appearance_mode("dark")
+
+
+
+
+
+
+
+
+app.mainloop()
